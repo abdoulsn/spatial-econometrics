c943fb50cd957622ca2a8f5f8bb1ef231b1d9394
abdoul <ablaye0m@gmail.com>
2021-03-15 17:28:14 +0100

    update

---
+++ moran_local.py
@@ -0,0 +1,22 @@
+li = esda.moran.Moran_Local(y, wq)
+
+sig = 1 * (li.p_sim < 0.05)
+hotspot = 1 * (sig * li.q==1)
+coldspot = 3 * (sig * li.q==3)
+doughnut = 2 * (sig * li.q==2)
+diamond = 4 * (sig * li.q==4)
+spots = hotspot + coldspot + doughnut + diamond
+
+# Labels
+spot_labels = [ '0 ns', '1 hot spot', '2 doughnut', '3 cold spot', '4 diamond']
+labels = [spot_labels[i] for i in spots]
+
+
+from matplotlib import colors
+hmap = colors.ListedColormap([ 'lightgrey', 'red', 'lightblue', 'blue', 'pink'])
+f, ax = plt.subplots(1, figsize=(9, 9))
+gdf.assign(cl=labels).plot(column='cl', categorical=True, \
+        k=2, cmap=hmap, linewidth=0.1, ax=ax, \
+        edgecolor='white', legend=True)
+ax.set_axis_off()
+plt.show()
